38858b6c89a46d247c72d9efc53f4b5b832bafc0
ssk <developer.kss@gmail.com>
2021-03-03 21:07:13 +0900

    lib 폴더 작동

--- test/willpop_admob_test.dart
+++ test/willpop_admob_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:willpop_admob/will_pop_admob.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('willpop_admob');
@@ -17,6 +18,5 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await WillpopAdmob.platformVersion, '42');
   });
 }
